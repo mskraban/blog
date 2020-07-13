0ae250af2ea3dd1a79669652e9580c9ff8c1166a
Slasher <mario.skraban@gmail.com>
2020-07-13 22:20:34 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/post/{id}/{name}', function ($id, $name){
+//    return "This is post number ". $id. " ". $name;
+//});
+//
+//Route::get('admin/posts/example', array('as'=>'admin.home', function(){
+//    $url = route('admin.home');
+//    return "this url is ". $url;
+//}));
+
+//Route::get('/post/{id}', 'PostsController@index');
+
+Route::resource('posts', 'PostsController');
+
+Route::get('/contact', 'PostsController@contact');
+
+Route::get('post/{id}/{name}/{password}', 'PostsController@show_post');
